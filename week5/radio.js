f7a74a6afdbcfa4a60b21f471352c45791ea8a17
Joseph Grigoletti <jgrig2@gmail.com>
2021-02-17 10:22:32 -0600

    week 6 homework

---
+++ week5/radio.js
@@ -0,0 +1,6 @@
+//Radio Commercials
+//Our favorite Onid Pizza would like to have a commercial aired in a radio. Since they are close to KTH, they want to attract mainly students. It’s not a good idea to have the commercial aired between 8 am and 5 pm, because most of the students are in the school and don’t listen to the radio. Onid made a survey and now they know how many students listen to the radio at each point of the day.
+//
+//They also know that if each student listens to a commercial, he or she will spend one Swedish crown on pizza in expectation. Thus if 100 students listen to a commercial, Onid will increase their income by 100 crowns on average from selling more pizza.
+//
+//Of course, Onid Pizza has to pay a fixed amount every time the commercial is played. The radio has a commercial break every 15 minutes. Unfortunately, Onid can choose only one continuous sequence of commercial breaks, for example all breaks from 5 pm to 8 pm. Help them to choose a continuous sequence of commercial breaks such that their profit is maximal.
